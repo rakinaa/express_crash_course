38cca766df5ec2f657c2f2ccedd9523529842764
rakin <rakinadm19@gmail.com>
2020-10-26 22:36:42 -0700

    added delete functionality

--- routes/api/members.js
+++ routes/api/members.js
@@ -57,7 +57,12 @@ router.delete("/:id", (req, res) => {
   const found = members.some((member) => member.id === parseInt(req.params.id));
 
   if (found) {
-    res.json(members.filter((member) => member.id === parseInt(req.params.id)));
+    res.json({
+      msg: "Member deleted",
+      members: members.filter(
+        (member) => member.id !== parseInt(req.params.id)
+      ),
+    });
   } else {
     res.status(400).json({ msg: `No member with id: ${req.params.id}` });
   }
