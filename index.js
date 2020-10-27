451d1685d47cf3fd34dec9b5a3082c37d10a7d7b
rakin <rakinadm19@gmail.com>
2020-10-26 23:00:58 -0700

    Added Views and completed course

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express = require("express");
 const path = require("path");
+const exphbs = require("express-handlebars");
 const logger = require("./middleware/logger");
+const members = require("./members");
 
 const app = express();
 
 // middleware
 // app.use(logger);
 
+// handlebars middleware
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Homepage route
+app.get("/", (req, res) =>
+  res.render("index", {
+    title: "Member App",
+    members,
+  })
+);
 
 // set static folder
 app.use(express.static(path.join(__dirname, "public")));
